e7f62b5568a2dd3ae1e7d6edf9cdf8e2cb571c02
Austin Hyde <austin109@gmail.com>
2012-07-30 10:06:08 -0400

    Add mysql5 index diffing logic and tests

---
+++ lib/DBSteward/sql_format/sql99/sql99_diff_indexes.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Diffs indexes.
+ *
+ * @package DBSteward
+ * @subpackage sql99
+ * @license http://www.opensource.org/licenses/bsd-license.php Simplified BSD License
+ * @author Austin Hyde <austin109@gmail.com>
+ */
+
+require_once __DIR__ . '/sql99_table.php';
+require_once __DIR__ . '/sql99_index.php';
+
+class sql99_diff_indexes {
+
+  /**
+   * Outputs commands for differences in indexes.
+   *
+   * @param $ofs       output file pointer
+   * @param old_schema original schema
+   * @param new_schema new schema
+   */
+  public static function diff_indexes($ofs, $old_schema, $new_schema) {
+    foreach(dbx::get_tables($new_schema) as $new_table) {
+      if ($old_schema == null) {
+        $old_table = null;
+      } else {
+        $old_table = dbx::get_table($old_schema, $new_table['name']);
+      }
+      static::diff_indexes_table($ofs, $old_schema, $old_table, $new_schema, $new_table);
+    }
+  }
+
+  public static function diff_indexes_table($ofs, $old_schema, $old_table, $new_schema, $new_table) {
+    // Drop indexes that do not exist in new schema or are modified
+    foreach(static::get_drop_indexes($old_schema, $old_table, $new_schema, $new_table) as $index) {
+      $ofs->write(static::get_index_drop_sql($new_schema, $new_table, $index)."\n");
+    }
+
+    // Add new indexes
+    if ($old_schema == null) {
+      foreach(dbx::get_table_indexes($new_schema, $new_table) as $index) {
+        $ofs->write(static::get_index_create_sql($new_schema, $new_table, $index)."\n");
+      }
+    } else {
+      foreach(static::get_new_indexes($old_schema, $old_table, $new_schema, $new_table) as $index) {
+        $ofs->write(static::get_index_create_sql($new_schema, $new_table, $index)."\n");
+      }
+    }
+  }
+
+  /**
+   * Returns list of indexes that should be dropped.
+   *
+   * @param old_table original table
+   * @param new_table new table
+   *
+   * @return list of indexes that should be dropped
+   *
+   * @todo Indexes that are depending on a removed field should not be added
+   *       to drop because they are already removed.
+   */
+  public static function get_drop_indexes($old_schema, $old_table, $new_schema, $new_table) {
+    $list = array();
+
+    if (($new_table != null) && ($old_table != null)) {
+      foreach(dbx::get_table_indexes($old_schema, $old_table) as $index) {
+        $old_index = dbx::get_table_index($new_schema, $new_table, $index['name']);
+        if ( !sql99_table::contains_index($new_schema, $new_table, $index['name']) ) {
+            $list[] = $index;
+        }
+        else if ( !sql99_index::equals($old_index, $index) ) {
+          $list[] = $index;
+        }
+      }
+    }
+
+    return $list;
+  }
+
+  /**
+   * Returns list of indexes that should be added.
+   *
+   * @param old_table original table
+   * @param new_table new table
+   *
+   * @return list of indexes that should be added
+   */
+  public static function get_new_indexes($old_schema, $old_table, $new_schema, $new_table) {
+    $list = array();
+
+    if ($new_table != null) {
+      if ($old_table == null) {
+        foreach(dbx::get_table_indexes($new_schema, $new_table) as $index) {
+          $list[] = $index;
+        }
+      } else {
+        foreach(dbx::get_table_indexes($new_schema, $new_table) as $index) {
+          $old_index = dbx::get_table_index($old_schema, $old_table, $index['name']);
+          if ( !sql99_table::contains_index($old_schema, $old_table, $index['name']) ) {
+            $list[] = $index;
+          }
+          else if ( !sql99_index::equals($old_index, $index) ) {
+            $list[] = $index;
+          }
+        }
+      }
+    }
+
+    return $list;
+  }
+
+  protected static function get_index_drop_sql($schema, $table, $index) {
+    throw new Exception('Must be overridden by specific sql_format driver');
+  }
+
+  protected static function get_index_create_sql($schema, $table, $index) {
+    throw new Exception('Must be overridden by specific sql_format driver');
+  }
+}
---
+++ tests/mysql5/Mysql5IndexDiffSQLTest.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * DBSteward unit test for mysql5 index diffing
+ *
+ * @package DBSteward
+ * @license http://www.opensource.org/licenses/bsd-license.php Simplified BSD License
+ * @author Austin Hyde <austin109@gmail.com>
+ */
+
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'PHPUnit/Framework/TestSuite.php';
+
+require_once __DIR__ . '/../../lib/DBSteward/dbsteward.php';
+require_once __DIR__ . '/../../lib/DBSteward/sql_format/mysql5/mysql5_diff_indexes.php';
+
+require_once __DIR__ . '/../mock_output_file_segmenter.php';
+
+class Mysql5IndexDiffSQLTest extends PHPUnit_Framework_TestCase {
+
+  public function setUp() {
+    dbsteward::set_sql_format('mysql5');
+    dbsteward::$quote_schema_names = TRUE;
+    dbsteward::$quote_table_names = TRUE;
+    dbsteward::$quote_column_names = TRUE;
+    dbsteward::$quote_function_names = TRUE;
+    dbsteward::$quote_object_names = TRUE;
+  }
+
+  private $xml_0 = <<<XML
+<schema name="test0" owner="NOBODY">
+  <table name="test" owner="NOBODY">
+    <column name="a"/>
+  </table>
+</schema>
+XML;
+
+  private $xml_1 = <<<XML
+<schema name="test1" owner="NOBODY">
+  <table name="test" owner="NOBODY">
+    <column name="a"/>
+    <index name="test_idxa">
+      <indexDimension>a</indexDimension>
+    </index>
+  </table>
+</schema>
+XML;
+
+  private $xml_1a = <<<XML
+<schema name="test1a" owner="NOBODY">
+  <table name="test" owner="NOBODY">
+    <column name="a"/>
+    <index name="test_idxa" using="hash">
+      <indexDimension>a</indexDimension>
+    </index>
+  </table>
+</schema>
+XML;
+
+  private $xml_1b = <<<XML
+<schema name="test1b" owner="NOBODY">
+  <table name="test" owner="NOBODY">
+    <column name="a"/>
+    <index name="test_idxa" unique="true">
+      <indexDimension>a</indexDimension>
+    </index>
+  </table>
+</schema>
+XML;
+
+  private $xml_3 = <<<XML
+<schema name="test3" owner="NOBODY">
+  <table name="test" owner="NOBODY">
+    <column name="a"/>
+    <column name="b"/>
+    <column name="c"/>
+    <index name="test_idxa">
+      <indexDimension>a</indexDimension>
+    </index>
+    <index name="test_idxb">
+      <indexDimension>b</indexDimension>
+    </index>
+    <index name="test_idxc">
+      <indexDimension>c</indexDimension>
+    </index>
+  </table>
+</schema>
+XML;
+
+  public function testNoneToNone() {
+    $this->common($this->xml_0, $this->xml_0, "");
+  }
+
+  public function testSameToSame() {
+    $this->common($this->xml_3, $this->xml_3, "");
+  }
+
+  public function testAddNew() {
+    $expected = <<<SQL
+CREATE INDEX `test_idxa` ON `test` (`a`);
+CREATE INDEX `test_idxb` ON `test` (`b`);
+CREATE INDEX `test_idxc` ON `test` (`c`);
+SQL;
+    $this->common($this->xml_0, $this->xml_3, $expected);
+  }
+
+  public function testAddSome() {
+    $expected = <<<SQL
+CREATE INDEX `test_idxb` ON `test` (`b`);
+CREATE INDEX `test_idxc` ON `test` (`c`);
+SQL;
+    $this->common($this->xml_1, $this->xml_3, $expected);
+  }
+
+  public function testDropAll() {
+    $expected = <<<SQL
+DROP INDEX `test_idxa` ON `test`;
+DROP INDEX `test_idxb` ON `test`;
+DROP INDEX `test_idxc` ON `test`;
+SQL;
+    $this->common($this->xml_3, $this->xml_0, $expected);
+  }
+
+  public function testDropSome() {
+    $expected = <<<SQL
+DROP INDEX `test_idxb` ON `test`;
+DROP INDEX `test_idxc` ON `test`;
+SQL;
+    $this->common($this->xml_3, $this->xml_1, $expected);
+  }
+
+  public function testChangeOne() {
+    $expected = <<<SQL
+DROP INDEX `test_idxa` ON `test`;
+CREATE UNIQUE INDEX `test_idxa` ON `test` (`a`);
+SQL;
+    $this->common($this->xml_1a, $this->xml_1b, $expected);
+  }
+
+  public function testAddSomeAndChange() {
+    $expected = <<<SQL
+DROP INDEX `test_idxa` ON `test`;
+CREATE INDEX `test_idxa` ON `test` (`a`);
+CREATE INDEX `test_idxb` ON `test` (`b`);
+CREATE INDEX `test_idxc` ON `test` (`c`);
+SQL;
+    $this->common($this->xml_1a, $this->xml_3, $expected);
+  }
+
+  public function testDropSomeAndChange() {
+    $expected = <<<SQL
+DROP INDEX `test_idxa` ON `test`;
+DROP INDEX `test_idxb` ON `test`;
+DROP INDEX `test_idxc` ON `test`;
+CREATE INDEX `test_idxa` ON `test` (`a`) USING HASH;
+SQL;
+    $this->common($this->xml_3, $this->xml_1a, $expected);
+  }
+
+  public function common($a, $b, $expected) {
+    $schema_a = new SimpleXMLElement($a);
+    $schema_b = new SimpleXMLElement($b);
+
+    $ofs = new mock_output_file_segmenter();
+
+    mysql5_diff_indexes::diff_indexes($ofs, $schema_a, $schema_b);
+
+    $actual = trim(preg_replace("/--.*\n/",'',$ofs->_get_output()));
+
+    $this->assertEquals($expected, $actual);
+  }
+}
