baed112c90e5b695d482e97e9375267013e6cd6b
Austin Hyde <austin109@gmail.com>
2012-08-07 16:21:34 -0400

    Adjust serialStartTest function definitions to match DTD

--- tests/serialStartTest.php
+++ tests/serialStartTest.php
@@ -38,10 +38,10 @@ class serialStartTest extends dbstewardUnitTestBase {
   </database>
   <language name="plpgsql" procedural="true" owner="ROLE_OWNER"/>
   <schema name="dbsteward" owner="ROLE_OWNER">
-    <function name="db_config_parameter" returns="text" language="plpgsql" owner="ROLE_OWNER" cachePolicy="VOLATILE" description="used to push configurationParameter values permanently into the database configuration">
+    <function name="db_config_parameter" returns="text" owner="ROLE_OWNER" cachePolicy="VOLATILE" description="used to push configurationParameter values permanently into the database configuration">
       <functionParameter name="config_parameter" type="text"/>
       <functionParameter name="config_value" type="text"/>
-      <functionDefinition>
+      <functionDefinition language="plpgsql" sqlFormat="pgsql8">
         DECLARE
           q text;
           name text;
@@ -93,10 +93,10 @@ XML;
   </database>
   <language name="plpgsql" procedural="true" owner="ROLE_OWNER"/>
   <schema name="dbsteward" owner="ROLE_OWNER">
-    <function name="db_config_parameter" returns="text" language="plpgsql" owner="ROLE_OWNER" cachePolicy="VOLATILE" description="used to push configurationParameter values permanently into the database configuration">
+    <function name="db_config_parameter" returns="text" owner="ROLE_OWNER" cachePolicy="VOLATILE" description="used to push configurationParameter values permanently into the database configuration">
       <functionParameter name="config_parameter" type="text"/>
       <functionParameter name="config_value" type="text"/>
-      <functionDefinition>
+      <functionDefinition language="plpgsql" sqlFormat="pgsql8">
         DECLARE
           q text;
           name text;
