3504b04a34c3de4efbe6134897ed1b39010af1f5
Austin Hyde <austin109@gmail.com>
2012-09-20 14:55:34 -0400

    Simplify extraction queries

--- lib/DBSteward/sql_format/mysql5/mysql5_db.php
+++ lib/DBSteward/sql_format/mysql5/mysql5_db.php
@@ -28,86 +28,86 @@ class mysql5_db {
   }
 
   public function get_tables() {
-    static $stmt;
-    if (!$stmt) {
-      $table_name = mysql5_sequence::TABLE_NAME;
-      // table_schema is database name. why? because mysql, that's why.
-      $stmt = $this->pdo->prepare("SELECT table_schema, table_name, engine,
-                                          table_rows, auto_increment, table_comment
-                                   FROM tables
-                                   WHERE table_type = 'base table'
-                                     AND table_name != '$table_name'
-                                     AND table_schema = ?");
-    }
-
-    $stmt->execute(array($this->dbname));
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    $table_name = mysql5_sequence::TABLE_NAME;
+    return $this->query("SELECT table_schema, table_name, engine,
+                                table_rows, auto_increment, table_comment
+                          FROM tables
+                          WHERE table_type = 'base table'
+                            AND table_name != '$table_name'
+                            AND table_schema = ?", array($this->dbname));
   }
 
   public function get_table($name) {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT table_schema, table_name, engine,
-                                          table_rows, auto_increment, table_comment
-                                   FROM tables
-                                   WHERE table_type = 'base table'
-                                     AND table_schema = ?
-                                     AND table_name = ?
-                                   LIMIT 1");
-    }
-
-    $stmt->execute(array($this->dbname, $name));
-    return $stmt->fetch(PDO::FETCH_OBJ);
+    return $this->query("SELECT table_schema, table_name, engine,
+                                table_rows, auto_increment, table_comment
+                         FROM tables
+                         WHERE table_type = 'base table'
+                           AND table_schema = ?
+                           AND table_name = ?
+                         LIMIT 1", array($this->dbname, $name), 'one');
   }
 
   public function uses_sequences() {
-    static $answer;
-    // cache the answer so we don't keep asking the DB stupid questions
-    if ($answer === null) {
-      $table_name = mysql5_sequence::TABLE_NAME;
-      $answer = $this->pdo->query("SELECT COUNT(*) FROM tables WHERE table_name = '$table_name'")->fetchColumn() != 0;
-    }
-    return $answer;
+    $table_name = mysql5_sequence::TABLE_NAME;
+    return $this->query("SELECT COUNT(*) FROM tables WHERE table_name = '$table_name'", array(), 'scalar') != 0;
   }
 
   public function get_sequences() {
-    static $stmt;
-
     if (!$this->uses_sequences()) {
       return array();
     }
 
-    if (!$stmt) {
-      $table_name = mysql5_sequence::TABLE_NAME;
-      $stmt = $this->pdo->prepare("SELECT * FROM `{$this->dbname}`.`$table_name`");
-    }
-
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    $table_name = mysql5_sequence::TABLE_NAME;
+    $name_col = mysql5_sequence::SEQ_COL;
+    return $this->query("SELECT *
+                           FROM `{$this->dbname}`.`$table_name`
+                          WHERE `$name_col` NOT LIKE '__public_%_%_serial_seq'");
   }
 
   public function get_columns($db_table) {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT table_name, column_name, column_default,
-                                          is_nullable, data_type, character_maximum_length, numeric_precision,
-                                          numeric_scale, column_type, column_key, extra, privileges,
-                                          extra = 'auto_increment' AS is_auto_increment
-                                   FROM columns
-                                   WHERE table_name = ?
-                                     AND table_schema = ?
-                                   ORDER BY ordinal_position ASC");
+    return $this->query("SELECT table_name, column_name, column_default,
+                                is_nullable, data_type, character_maximum_length, numeric_precision,
+                                numeric_scale, column_type, column_key, extra, privileges,
+                                extra = 'auto_increment' AS is_auto_increment
+                         FROM columns
+                         WHERE table_name = ?
+                           AND table_schema = ?
+                         ORDER BY ordinal_position ASC",array($db_table->table_name, $this->dbname));
+  }
+
+  public function is_serial_column($db_table, $db_column) {
+    static $seq_stmt, $trig_stmt;
+    
+    if (!$this->uses_sequences()) {
+      // if we're not using sequences, it can't possibly be a serial column
+      return false;
     }
 
-    $stmt->execute(array($db_table->table_name, $this->dbname));
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    $table_name = mysql5_sequence::TABLE_NAME;
+    $name_col = mysql5_sequence::SEQ_COL;
+
+    // @TODO: unify with mysql5_column::get_serial_sequence_name()
+    $seq_name = '__public_' . $db_table->table_name . '_' . $db_column->column_name . '_serial_seq';
+    $has_seq = $this->query("SELECT COUNT(*) = 1
+                               FROM `{$this->dbname}`.`$table_name`
+                              WHERE `$name_col` = ?", array($seq_name), 'scalar');
+
+    // @TODO: unify with mysql5_column::get_serial_trigger_name()
+    $trig_name = '__public_' . $db_table->table_name . '_' . $db_column->column_name . '_serial_trigger';
+    $has_trig = $this->query("SELECT COUNT(*) = 1
+                                FROM triggers
+                               WHERE trigger_schema = ?
+                                 AND trigger_name = ?", array($this->dbname, $trig_name), 'scalar');
+
+    if ($has_seq && $has_trig) {
+      return strcasecmp($db_column->data_type,'bigint')===0 ? 'bigserial' : 'serial';
+    }
+    return false;
   }
 
   public function get_indices($db_table) {
     // only show those indexes which are not keys/constraints, unless it's a unique constraint
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT statistics.table_name,
+    $indices = $this->query("SELECT statistics.table_name,
                                           GROUP_CONCAT(statistics.column_name ORDER BY seq_in_index) AS columns,
                                           NOT statistics.non_unique AS 'unique', statistics.index_name,
                                           statistics.nullable, statistics.comment, statistics.index_type
@@ -118,12 +118,7 @@ class mysql5_db {
                                      AND statistics.table_name = ?
                                      AND (key_column_usage.constraint_name IS NULL 
                                           OR table_constraints.constraint_type = 'UNIQUE')
-                                   GROUP BY index_name");
-    }
-
-    $stmt->execute(array($this->dbname, $db_table->table_name));
-    $indices = $stmt->fetchAll(PDO::FETCH_OBJ);
-
+                                   GROUP BY index_name", array($this->dbname, $db_table->table_name));
     foreach ($indices as &$idx) {
       // massage the output
       $idx->columns = explode(',',$idx->columns);
@@ -133,27 +128,21 @@ class mysql5_db {
   }
 
   public function get_constraints($db_table) {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT GROUP_CONCAT(column_name ORDER BY position_in_unique_constraint, seq_in_index) AS columns,
-                                          statistics.table_name, table_constraints.constraint_name, index_name, constraint_type, key_column_usage.referenced_table_name,
-                                          GROUP_CONCAT(referenced_column_name ORDER BY position_in_unique_constraint) as referenced_columns,
-                                          update_rule, delete_rule
-                                    FROM statistics
-                                     INNER JOIN key_column_usage USING (table_schema, table_name, column_name)
-                                     INNER JOIN table_constraints USING (table_schema, table_name, constraint_name)
-                                     LEFT OUTER JOIN referential_constraints
-                                             ON referential_constraints.constraint_schema = statistics.table_schema
-                                            AND referential_constraints.constraint_name = table_constraints.constraint_name
-                                            AND referential_constraints.table_name = statistics.table_name
-                                    WHERE statistics.table_schema = ?
-                                     AND statistics.table_name = ?
-                                     AND (table_constraints.constraint_type != 'UNIQUE')
-                                    GROUP BY index_name;");
-    }
-
-    $stmt->execute(array($this->dbname, $db_table->table_name));
-    $constraints = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $constraints = $this->query("SELECT GROUP_CONCAT(column_name ORDER BY position_in_unique_constraint, seq_in_index) AS columns,
+                                        statistics.table_name, table_constraints.constraint_name, index_name, constraint_type, key_column_usage.referenced_table_name,
+                                        GROUP_CONCAT(referenced_column_name ORDER BY position_in_unique_constraint) as referenced_columns,
+                                        update_rule, delete_rule
+                                  FROM statistics
+                                   INNER JOIN key_column_usage USING (table_schema, table_name, column_name)
+                                   INNER JOIN table_constraints USING (table_schema, table_name, constraint_name)
+                                   LEFT OUTER JOIN referential_constraints
+                                           ON referential_constraints.constraint_schema = statistics.table_schema
+                                          AND referential_constraints.constraint_name = table_constraints.constraint_name
+                                          AND referential_constraints.table_name = statistics.table_name
+                                  WHERE statistics.table_schema = ?
+                                   AND statistics.table_name = ?
+                                   AND (table_constraints.constraint_type != 'UNIQUE')
+                                  GROUP BY index_name;", array($this->dbname, $db_table->table_name));
 
     foreach ($constraints as &$constraint) {
       // massage the output
@@ -168,91 +157,62 @@ class mysql5_db {
   }
 
   public function get_table_grants($db_table, $username) {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT grantee, GROUP_CONCAT(privilege_type) as operations, is_grantable = 'YES' as is_grantable
-                                   FROM table_privileges
-                                   WHERE table_schema = ?
-                                     AND table_name = ?
-                                     AND grantee LIKE ?
-                                   GROUP BY is_grantable");
-    }
-
-    $stmt->execute(array($this->dbname, $db_table->table_name, "'$username'@%"));
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $this->query("SELECT grantee, GROUP_CONCAT(privilege_type) as operations, is_grantable = 'YES' as is_grantable
+                           FROM table_privileges
+                           WHERE table_schema = ?
+                             AND table_name = ?
+                             AND grantee LIKE ?
+                           GROUP BY is_grantable", array($this->dbname, $db_table->table_name, "'$username'@%"));
   }
 
   public function get_global_grants($username) {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT grantee, GROUP_CONCAT(DISTINCT privilege_type ORDER BY privilege_type ASC) AS operations, COUNT(DISTINCT privilege_type) AS num_ops, is_grantable='YES' as is_grantable FROM (
-                                    SELECT grantee, privilege_type, is_grantable FROM schema_privileges WHERE grantee LIKE :grantee AND table_schema = :schema
-                                    UNION
-                                    SELECT grantee, privilege_type, is_grantable FROM user_privileges WHERE grantee LIKE :grantee
-                                  ) AS tbl
-                                  GROUP BY is_grantable;");
-    }
-    $stmt->execute(array('schema'=>$this->dbname, 'grantee'=>"'$username'@%"));
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $this->query("SELECT grantee, GROUP_CONCAT(DISTINCT privilege_type ORDER BY privilege_type ASC) AS operations, COUNT(DISTINCT privilege_type) AS num_ops, is_grantable='YES' as is_grantable FROM (
+                          SELECT grantee, privilege_type, is_grantable FROM schema_privileges WHERE grantee LIKE :grantee AND table_schema = :schema
+                          UNION
+                          SELECT grantee, privilege_type, is_grantable FROM user_privileges WHERE grantee LIKE :grantee
+                        ) AS tbl
+                        GROUP BY is_grantable;", array('schema'=>$this->dbname, 'grantee'=>"'$username'@%"));
   }
 
   public function get_functions() {
-    static $fn_stmt, $param_stmt;
-    if (!$fn_stmt) {
-      if ($this->uses_sequences()) {
-        $not_shim_fns = "AND routine_name NOT IN ('nextval','setval','currval','lastval')";
-      }
-      else {
-        $not_shim_fns = "";
-      }
-      $fn_stmt = $this->pdo->prepare("SELECT routine_name, data_type, character_maximum_length, numeric_precision, routine_comment,
-                                          numeric_scale, routine_definition, is_deterministic, security_type, definer, sql_data_access, dtd_identifier
-                                   FROM routines
-                                   WHERE routine_type = 'FUNCTION' $not_shim_fns
-                                     AND routine_schema = ?");
-      $param_stmt = $this->pdo->prepare("SELECT parameter_mode, parameter_name, data_type, character_maximum_length,
-                                                numeric_precision, numeric_scale, dtd_identifier
-                                         FROM parameters
-                                         WHERE specific_schema = ?
-                                           AND specific_name = ?
-                                           AND parameter_name IS NOT NULL
-                                         ORDER BY ordinal_position ASC");
+    if ($this->uses_sequences()) {
+      $not_shim_fns = "AND routine_name NOT IN ('nextval','setval','currval','lastval')";
     }
-    $fn_stmt->execute(array($this->dbname));
-
-    $functions = array();
-    foreach ($fn_stmt->fetchAll(PDO::FETCH_OBJ) as $fn) {
-      $param_stmt->execute(array($this->dbname, $fn->routine_name));
-      $fn->parameters = $param_stmt->fetchAll(PDO::FETCH_OBJ);
-      $functions[] = $fn;
+    else {
+      $not_shim_fns = "";
+    }
+    $functions = $this->query("SELECT routine_name, data_type, character_maximum_length, numeric_precision, routine_comment,
+                                      numeric_scale, routine_definition, is_deterministic, security_type, definer, sql_data_access, dtd_identifier
+                               FROM routines
+                               WHERE routine_type = 'FUNCTION' $not_shim_fns
+                                 AND routine_schema = ?", array($this->dbname));
+    foreach ($functions as $fn) {
+      $fn->parameters = $this->query("SELECT parameter_mode, parameter_name, data_type, character_maximum_length,
+                                                    numeric_precision, numeric_scale, dtd_identifier
+                                             FROM parameters
+                                             WHERE specific_schema = ?
+                                               AND specific_name = ?
+                                               AND parameter_name IS NOT NULL
+                                             ORDER BY ordinal_position ASC", array($this->dbname, $fn->routine_name));
     }
 
     return $functions;
   }
 
   public function get_triggers() {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT trigger_name AS name, event_manipulation AS event, 
-                                          event_object_table AS 'table', action_statement AS function,
-                                          action_orientation AS forEach, action_timing AS 'when'
-                                   FROM triggers
-                                   WHERE trigger_schema = ?");
-    }
-    $stmt->execute(array($this->dbname));
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $this->query("SELECT trigger_name AS name, event_manipulation AS event, 
+                                event_object_table AS 'table', action_statement AS function,
+                                action_orientation AS forEach, action_timing AS 'when'
+                         FROM triggers
+                         WHERE trigger_schema = ?
+                           AND trigger_name NOT LIKE '__public_%_%_serial_trigger'", array($this->dbname));
   }
 
   public function get_views() {
-    static $stmt;
-    if (!$stmt) {
-      $stmt = $this->pdo->prepare("SELECT table_name AS view_name, view_definition AS view_query,
-                                          definer, 'DEFINER' AS security_type
-                                   FROM views
-                                   WHERE table_schema = ?");
-    }
-    $stmt->execute(array($this->dbname));
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $this->query("SELECT table_name AS view_name, view_definition AS view_query,
+                                        definer, 'DEFINER' AS security_type
+                                 FROM views
+                                 WHERE table_schema = ?", array($this->dbname));
   }
 
   public function parse_enum_values($enum) {
@@ -262,6 +222,24 @@ class mysql5_db {
     }
     else {
       throw new Exception("Invalid enum declaration: $enum");
+    }
+  }
+
+  private function query($sql, $params = array(), $type='all') {
+    static $stmts = array();
+    if (!array_key_exists($sql, $stmts)) {
+      $stmts[$sql] = $this->pdo->prepare($sql);
+      if (!$stmts[$sql]) {
+        throw new Exception("Could not prepare sql:\n$sql");
+      }
+    }
+
+    $stmts[$sql]->execute($params);
+
+    switch ($type) {
+      case 'one': return $stmts[$sql]->fetch(PDO::FETCH_OBJ);
+      case 'scalar': return $stmts[$sql]->fetchColumn();
+      default: return $stmts[$sql]->fetchAll(PDO::FETCH_OBJ);
     }
   }
 }
