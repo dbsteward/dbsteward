38ca14a0e03ee4cb43d7efcd206944a465b94551
Austin Hyde <austin109@gmail.com>
2012-11-28 17:34:27 -0500

    Add --quoteallnames flag, variable, and logic

--- lib/DBSteward/sql_format/sql99/sql99.php
+++ lib/DBSteward/sql_format/sql99/sql99.php
@@ -80,6 +80,8 @@ class sql99 {
    * @return string
    */
   public static function get_quoted_name($name, $quoted, $quote_char) {
+    $quoted = $quoted || dbsteward::$quote_all_names;
+    
     if ( preg_match('/^[a-zA-Z_]\w*$/', $name) == 0 ) {
       if ( $quoted && preg_match('/^\w*$/', $name) > 0 ) {
         // if the identifier is to be quoted, it can start with a number
