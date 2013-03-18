d26f3bb7ecbb661a3637a6b6e1cf87e74c3140ad
Austin Hyde <austin109@gmail.com>
2013-03-18 15:59:49 -0400

    Use database-qualified table names

--- lib/DBSteward/sql_format/mysql5/mysql5.php
+++ lib/DBSteward/sql_format/mysql5/mysql5.php
@@ -840,11 +840,11 @@ class mysql5 {
   }
 
   public static function get_fully_qualified_table_name($schema_name, $table_name) {
-    return static::get_quoted_table_name($table_name);
+    return self::get_quoted_schema_name($schema_name) . '.' . self::get_quoted_table_name($table_name);
   }
 
   public static function get_fully_qualified_column_name($schema_name, $table_name, $column_name) {
-    return static::get_fully_qualified_table_name($schema_name, $table_name) . '.' . static::get_quoted_column_name($column_name);
+    return self::get_fully_qualified_table_name($schema_name, $table_name) . '.' . self::get_quoted_column_name($column_name);
   }
 
   // @TODO: pull up and generalize
