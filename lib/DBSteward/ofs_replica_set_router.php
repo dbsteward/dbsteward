5656520a10825f0188151dc3d38b300e3b46cf50
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2013-06-03 17:26:18 -0400

    append_header should be run on all ofs managed by the ofs router

--- lib/DBSteward/ofs_replica_set_router.php
+++ lib/DBSteward/ofs_replica_set_router.php
@@ -29,18 +29,40 @@ class ofs_replica_set_router {
    * @return object
    */
   public function set_ofs($set_id, $ofs) {
+    $set_id = (string)$set_id;
     $this->ofs[$set_id] = $ofs;
     format::$current_replica_set_id = $set_id;
     return $this->ofs[$set_id];
   }
   
   public function __call($m, $a) {
+    $ignore_ofs_methods = array(
+      '__destruct'
+    );
+    if ( in_array($m, $ignore_ofs_methods) ) {
+      return 'IGNORE_OFS_COMMAND_COMPLETE';
+    }
+
+    // if the command is in the list of commands to run on all ofs objects, do so
+    $all_ofs_methods = array(
+      'append_header',
+      'append_footer'
+    );
+    if ( in_array($m, $all_ofs_methods) ) {
+      foreach($this->ofs AS $ofs) {
+        call_user_func_array(array(&$ofs, $m), $a);
+      }
+      return 'ALL_OFS_COMMAND_COMPLETE';
+    }
+
     if ( !isset($this->ofs[format::$current_replica_set_id]) ) {
       if ( $this->ignore_unknown_set_ids ) {
+        //dbsteward::console_line(7, "ofs replica_set_id " . format::$current_replica_set_id . " not defined, skipping ofsr call");
         return FALSE;
       }
       throw new exception("current_replica_set_id " . format::$current_replica_set_id . " not defined");
     }
+
     $active_set_ofs = $this->ofs[format::$current_replica_set_id];
     return call_user_func_array(array(&$active_set_ofs, $m), $a);
   }
