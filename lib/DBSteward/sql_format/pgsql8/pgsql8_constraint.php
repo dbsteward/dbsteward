af17a0fc53ec77f177570a8ac6ed766fc9e61a45
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2013-06-26 11:50:48 -0400

    warn code maintainers about the partial implementation of pgsql8_constraint

--- lib/DBSteward/sql_format/pgsql8/pgsql8_constraint.php
+++ lib/DBSteward/sql_format/pgsql8/pgsql8_constraint.php
@@ -9,7 +9,21 @@
  */
 
 class pgsql8_constraint extends sql99_constraint {
+  
+  /*
+   * @IMPORTANT:
+   * @NOTICE:
+   * @TODO:
+   * 
+   * This implementation class is INCOMPLETE and only implemented in a few places in DBSteward code!
+   * pgsql8_table and pgsql8_column still need conversion work to use pgsql8_constraint fully
+   */
 
+  /**
+   * Filter the REFERENCES flag modified SQL from DBSteward DTD enumeration to SQL dialect
+   * @param string $ref_opt
+   * @return string
+   */
   public static function get_reference_option_sql($ref_opt) {
     return strtoupper(str_replace('_',' ',$ref_opt));
   }
