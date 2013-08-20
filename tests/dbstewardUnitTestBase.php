e945b355ffdbaebc2f9f36a2c747d20bb59e1260
Austin Hyde <austin109@gmail.com>
2013-08-20 14:42:42 -0400

    Fix mysql5 options to be consistent with what unit tests expect

--- tests/dbstewardUnitTestBase.php
+++ tests/dbstewardUnitTestBase.php
@@ -171,6 +171,8 @@ class dbstewardUnitTestBase extends PHPUnit_Framework_TestCase {
     dbsteward::$quote_schema_names = TRUE;
     dbsteward::$quote_table_names = TRUE;
     dbsteward::$quote_column_names = TRUE;
+    dbsteward::$quote_all_names = TRUE;
+    mysql5::$swap_function_delimiters = TRUE;
   }
   
   protected function build_db_mysql5() {
