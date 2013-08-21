ea9e7b2f64877e8f73283925f7c4a72c3de51453
Austin Hyde <austin109@gmail.com>
2013-08-21 09:31:56 -0400

    Command line args were all messed up

--- tests/dbsteward_mysql5_connection.php
+++ tests/dbsteward_mysql5_connection.php
@@ -13,7 +13,7 @@ class dbsteward_mysql5_connection extends dbsteward_sql99_connection {
 
   public function query($sql, $throw_on_error = TRUE) {
     dbsteward::cmd(sprintf("mysql -v --host=%s --port=%s --user=%s --database=%s --password=%s -e %s",
-                          $sql, $this->dbhost, $this->dbport, $this->dbname, $this->dbuser, $this->dbpass, escapeshellarg($sql)));
+                          $this->dbhost, $this->dbport, $this->dbuser, $this->dbname, $this->dbpass, escapeshellarg($sql)));
   }
 
   /**
@@ -24,7 +24,7 @@ class dbsteward_mysql5_connection extends dbsteward_sql99_connection {
     $sql = sprintf('DROP DATABASE IF EXISTS %1$s; CREATE DATABASE %1$s; GRANT ALL ON %1$s.* to %2$s WITH GRANT OPTION;', 
             $this->dbname, $this->dbuser);
     dbsteward::cmd(sprintf("mysql -v --host=%s --port=%s --user=%s --database=%s --password=%s -e %s",
-                          $this->dbhost, $this->dbport, $this->dbname_mgmt, $this->dbuser_mgmt, $this->dbpass_mgmt, escapeshellarg($sql)));
+                          $this->dbhost, $this->dbport, $this->dbuser_mgmt, $this->dbname_mgmt, $this->dbpass_mgmt, escapeshellarg($sql)));
   }
 
   protected function pipe_file_to_client($file_name) {
