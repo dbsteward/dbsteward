53b15553c8681741e8c7cfa98913e9c3eedaac7e
Austin Hyde <austin109@gmail.com>
2013-08-21 16:39:42 -0400

    Quote the target of the create_db grant in mysql5

--- tests/dbsteward_mysql5_connection.php
+++ tests/dbsteward_mysql5_connection.php
@@ -21,7 +21,7 @@ class dbsteward_mysql5_connection extends dbsteward_sql99_connection {
    * @return void
    */
   public function create_db() {
-    $sql = sprintf('DROP DATABASE IF EXISTS %1$s; CREATE DATABASE %1$s; GRANT ALL ON %1$s.* to %2$s WITH GRANT OPTION;', 
+    $sql = sprintf('DROP DATABASE IF EXISTS %1$s; CREATE DATABASE %1$s; GRANT ALL ON %1$s.* to \'%2$s\' WITH GRANT OPTION;', 
             $this->dbname, $this->dbuser);
     dbsteward::cmd(sprintf("mysql -v --host=%s --port=%s --user=%s --database=%s --password=%s -e %s",
                           $this->dbhost, $this->dbport, $this->dbuser_mgmt, $this->dbname_mgmt, $this->dbpass_mgmt, escapeshellarg($sql)));
