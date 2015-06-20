49877ba8a0d805c372a6e03beb12c1a99d7f44dc
Austin Hyde <austin109@gmail.com>
2015-06-20 18:00:11 -0400

    Add better coloring, handle exceptions better

--- lib/DBSteward/DBStewardConsoleLogFormatter.php
+++ lib/DBSteward/DBStewardConsoleLogFormatter.php
@@ -1,21 +1,28 @@
 <?php
 
 use Monolog\Logger;
+use Colors\Color;
 
 class DBStewardConsoleLogFormatter extends Monolog\Formatter\LineFormatter {
   public function __construct() {
-    parent::__construct('%level_name% %message%', null, true, false);
+    parent::__construct('%padded_level% %message%', null, true, false);
   }
   public function format(array $record) {
+    $record['padded_level'] = str_pad($record['level_name'], 8);
+    $record['message'] = str_replace("\n", "\n         ", $record['message']);
     $output = parent::format($record);
+    $c = new Color($output);
     switch ($record['level']) {
+      case Logger::DEBUG:
+        $c->dark_gray();
+        break;
       case Logger::WARNING:
-        $output = "\033[33m" . $output . "\033[0m";
+        $c->yellow();
         break;
       case Logger::ERROR:
-        $output = "\033[31m" . $output . "\033[0m";
+        $c->red();
         break;
     }
-    return $output."\n";
+    return $c . PHP_EOL;
   }
 }
