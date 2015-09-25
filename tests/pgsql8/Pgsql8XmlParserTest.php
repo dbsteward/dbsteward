37dbf869577045a0dfa68f5bfde3ca775d67a57d
Brian Stoots <bstoots@gmail.com>
2015-09-25 18:09:50 -0400

    Fix indexWhere for partition tables.  Add covering unit tests.

---
+++ tests/pgsql8/Pgsql8XmlParserTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * DBSteward unit tests for pgsql8_xml_parser
+ *
+ * @package DBSteward
+ * @license http://www.opensource.org/licenses/bsd-license.php Simplified BSD License
+ * @author Brian Stoots <bstoots@gmail.com>
+ */
+
+require_once __DIR__ . '/../dbstewardUnitTestBase.php';
+
+/**
+ * @group pgsql8
+ */
+class Pgsql8XmlParserTest extends dbstewardUnitTestBase {
+  
+  /**
+   * @dataProvider processProvider()
+   */
+  public function testProcess($in_xml, $out_xml) {
+    $doc = new SimpleXMLElement(file_get_contents($in_xml));
+    pgsql8_xml_parser::process($doc);
+    // Uncomment this line to create the _out file that will be asserted against.
+    // Make sure to double check the contents and then commit to version control
+    //file_put_contents($out_xml, $doc->saveXML());
+    $this->assertEquals(new SimpleXMLElement(file_get_contents($out_xml)), $doc);
+  }
+
+  /**
+   * 
+   */
+  public static function processProvider() {
+    $dir = static::getTestDataDir();
+    return array(
+      array("{$dir}/pgsql8/partition/partition_001_in.xml", "{$dir}/pgsql8/partition/partition_001_out.xml"),
+      array("{$dir}/pgsql8/partition/partition_002_in.xml", "{$dir}/pgsql8/partition/partition_002_out.xml"),
+      array("{$dir}/pgsql8/partition/partition_003_in.xml", "{$dir}/pgsql8/partition/partition_003_out.xml"),
+    );
+  }
+
+}
