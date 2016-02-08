65d39bdc7f0606cc85cd621649911be2b114e823
Aurimas Valevicius <aurimas.valevicius@intermedix.com>
2016-02-08 10:55:24 +0200

    small fix

--- bin/dbsteward.bat
+++ bin/dbsteward.bat
@@ -14,34 +14,14 @@ set APP_HOME=%DIRNAME%..
 goto init
 
 :init
-@rem Get command-line arguments, handling Windowz variants
-if not "%OS%" == "Windows_NT" goto win9xME_args
-if "%@eval[2+2]" == "4" goto 4NT_args
-
-:win9xME_args
-@rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
-
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
-
 set CMD_LINE_ARGS=%*
 goto execute
 
-:4NT_args
-@rem Get arguments from the 4NT Shell from JP Software
-set CMD_LINE_ARGS=%$
-
 :execute
-@php %DBSTEWARD_HOME%\bin\dbsteward %CMD_LINE_ARGS%
 php %DIRNAME%\dbsteward %CMD_LINE_ARGS%
 
 :end
-@rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
-
-:omega
