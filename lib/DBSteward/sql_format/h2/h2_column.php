458c4d6d4cda6ced1b6fe1d1b95b45e06915e7f7
Aurimas Valevicius <aurimas.valevicius@intermedix.com>
2016-03-07 13:25:22 +0200

    "ON UPDATE CURRENT_TIMESTAMP" -> AS CURRENT_TIMESTAMP

--- lib/DBSteward/sql_format/h2/h2_column.php
+++ lib/DBSteward/sql_format/h2/h2_column.php
@@ -8,6 +8,11 @@ class h2_column extends sql99_column
         return stripos($node_column['type'], 'timestamp') === 0;
     }
 
+    public static function is_current_timestamp_on_update($node_column)
+    {
+        return static::is_timestamp($node_column) && stripos($node_column['type'], 'ON UPDATE CURRENT_TIMESTAMP') !== FALSE;
+    }
+
     public static function null_allowed($node_table, $node_column)
     {
         if (static::is_serial($node_column['type'])) {
@@ -20,7 +25,7 @@ class h2_column extends sql99_column
         }
     }
 
-    public static function  get_full_definition($db_doc, $node_schema, $node_table, $node_column, $add_defaults, $include_null_definition = true, $include_auto_increment = false)
+    public static function get_full_definition($db_doc, $node_schema, $node_table, $node_column, $add_defaults, $include_null_definition = true, $include_auto_increment = false)
     {
         // ignore AUTO_INCREMENT flags for now
         $is_auto_increment = static::is_auto_increment($node_column['type']);
@@ -28,21 +33,13 @@ class h2_column extends sql99_column
         $node_column['type'] = static::un_auto_increment($node_column['type']);
 
         $column_type = static::column_type($db_doc, $node_schema, $node_table, $node_column);
-
         $definition = h2::get_quoted_column_name($node_column['name']) . ' ' . $column_type;
-
         $nullable = static::null_allowed($node_table, $node_column);
-
         $is_timestamp = static::is_timestamp($node_column);
+        $is_current_timestamp_on_update = static::is_current_timestamp_on_update($node_column);
 
-        if ($include_null_definition) {
-            if ($nullable) {
-                if ($is_timestamp) {
-                    $definition .= " NULL";
-                }
-            } else {
-                $definition .= " NOT NULL";
-            }
+        if ($include_null_definition && !$nullable) {
+            $definition .= " NOT NULL";
         }
 
         if ($include_auto_increment && $is_auto_increment) {
@@ -54,21 +51,31 @@ class h2_column extends sql99_column
                 $note = "Ignoring default '{$node_column['default']}' on {$node_schema['name']}.{$node_table['name']}.{$node_column['name']} because it is a serial type";
                 dbsteward::warning($note . "\n");
             } else {
-                $definition .= " DEFAULT " . $node_column['default'];
+                if ($is_current_timestamp_on_update) {
+                    $definition = static::current_timestamp_on_update_for_h2($node_column, $definition);
+                } else {
+                    $definition .= " DEFAULT " . $node_column['default'];
+                }
             }
-        } else if ($add_defaults && $is_timestamp) {
-            if ($nullable) {
-                $definition .= " DEFAULT NULL";
-            } else {
-                $definition .= " DEFAULT CURRENT_TIMESTAMP";
-            }
-        } else if (!$nullable && $add_defaults) {
-            ;
-            $default_col_value = self::get_default_value($node_column['type']);
-            if ($default_col_value != null) {
-                $definition .= " DEFAULT " . $default_col_value;
-            }
-        }
+        } else
+            if ($add_defaults && $is_timestamp) {
+                if ($nullable) {
+                    $definition .= " DEFAULT NULL";
+                } else {
+                    if ($is_current_timestamp_on_update) {
+                        $definition = static::current_timestamp_on_update_for_h2($node_column, $definition);
+                    } else {
+                        $definition .= " DEFAULT CURRENT_TIMESTAMP";
+                    }
+                }
+            } else
+                if (!$nullable && $add_defaults) {
+                    ;
+                    $default_col_value = self::get_default_value($node_column['type']);
+                    if ($default_col_value != null) {
+                        $definition .= " DEFAULT " . $default_col_value;
+                    }
+                }
 
         if (strlen($node_column['description']) > 0) {
             $definition .= " COMMENT " . h2::quote_string_value($node_column['description']);
@@ -123,12 +130,7 @@ class h2_column extends sql99_column
 
         // remove zerofill
         if (strpos(strtolower($type), "zerofill")) {
-            $type =  str_replace("zerofill", "", $type);
-        }
-
-        // clean timestamp
-        if (strpos(strtolower($type), "timestamp") !== FALSE) {
-            $type = "timestamp";
+            $type = str_replace("zerofill", "", $type);
         }
 
         // change enums to varchar
@@ -157,5 +159,15 @@ class h2_column extends sql99_column
         return "SELECT $setval;";
     }
 
+    // AS CURRENT_TIMESTAMP only updates timestamp, and not allows to insert specific value.
+    // There is no better solution for H2 with this issue
+    public static function current_timestamp_on_update_for_h2($node_column, $definition)
+    {
+        $definition = str_replace("ON UPDATE CURRENT_TIMESTAMP", '', $definition);
+        $definition .= " AS " . $node_column['default'];
+        return $definition;
+    }
+
 }
+
 ?>
