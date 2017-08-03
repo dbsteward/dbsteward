917d2b1b622cc322861df7428c9f1f16ba9ef6d8
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2017-08-02 23:49:24 -0400

    dbsteward.bat use case typo

--- bin/dbsteward.bat
+++ bin/dbsteward.bat
@@ -7,7 +7,7 @@ REM @package DBSteward
 REM @license http://www.opensource.org/licenses/bsd-license.php Simplified BSD License
 REM @author Nicholas J Kiraly <kiraly.nicholas@gmail.com>
 REM
-REM This batch file is to streamline runnign dbsteward from a repository checkout on windows.
+REM This batch file is to streamline running dbsteward from a repository checkout on windows.
 REM Note that #126 describes how composer does this for package installations automatically
 REM https://getcomposer.org/doc/articles/vendor-binaries.md#what-about-windows-and-bat-files-
 
