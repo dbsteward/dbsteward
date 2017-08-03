f83600f951d760235e9eda6a82ed8a34e92dad2c
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2017-08-02 23:48:18 -0400

    update dbsteward.bat intended use case and notes from #126 regarding composer package automatic generation of similar

--- bin/dbsteward.bat
+++ bin/dbsteward.bat
@@ -6,5 +6,9 @@ REM
 REM @package DBSteward
 REM @license http://www.opensource.org/licenses/bsd-license.php Simplified BSD License
 REM @author Nicholas J Kiraly <kiraly.nicholas@gmail.com>
+REM
+REM This batch file is to streamline runnign dbsteward from a repository checkout on windows.
+REM Note that #126 describes how composer does this for package installations automatically
+REM https://getcomposer.org/doc/articles/vendor-binaries.md#what-about-windows-and-bat-files-
 
 php %~dp0\dbsteward %*
